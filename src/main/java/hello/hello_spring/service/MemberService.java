b992cf64c8e59114cc14a40b517e5a1836b2a5be
yeseul <yeseul0226@ewhain.net>
2025-05-09 17:37:32 +0900

    initial commit

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,6 +3,8 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +12,7 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
