8e6d1af4b965fded364c2aed14fa9ffee49a2958
yeseul <yeseul0226@ewhain.net>
2025-05-17 23:54:42 +0900

    9w

--- src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
